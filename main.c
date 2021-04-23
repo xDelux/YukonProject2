63627252ab845deb1854c8d63bd4084d766a575c
sluxh <s195467@student.dtu.dk>
2021-04-23 13:32:47 +0200

    console print and Card struct

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+char input[];
+
+struct Card {
+    char value;
+    char type;
+    int hidden;
+    struct Card* next;
+    struct Card* prev;
+};
+
+void printConsole();
+void commandLD();
+
 int main(void) {
-    printf("Hello World!\n");
+    printConsole();
+    scanf("%s", &input);
+
+}
+
+void printConsole() {
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\t\t\t\n\n");
+    printf("\t\t\t\t\t\t\t[]\tF1\n");
+    printf("\n");
+    printf("\t\t\t\t\t\t\t[]\tF2\n");
+    printf("\n");
+    printf("\t\t\t\t\t\t\t[]\tF3\n");
+    printf("\n");
+    printf("\t\t\t\t\t\t\t[]\tF4\n");
+    printf("LAST Command: \n");
+    printf("Message: \n");
+    printf("INPUT > ");
 }
